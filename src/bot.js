0026ea13215a0445410ddc25926c9f2e4c5618e2
Andrew Graham <asg8626@gmail.com>
2021-07-26 23:25:01 -0500

    remove env again

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,9 @@
 const tmi = require('tmi.js');
 require('dotenv').config();
-let { humor } = require('../store')
+const fs = require('fs');
+let humorRaw = fs.readFileSync('store/index.json');
+let store = JSON.parse(humorRaw);
+
 // Define configuration options
 const opts = {
   identity: {
@@ -22,9 +25,9 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
-setInterval(() => {
-  console.log("current humor", humor)
-}, 5000)
+//save score every 60 seconds
+setInterval(saveHumor, 60000)
+
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
@@ -34,15 +37,25 @@ function onMessageHandler (target, context, msg, self) {
 
   // If the command is known, let's execute it
   let op = commandName.slice(0,1)
-  console.log(msg, humor)
   if (['+','-'].includes(op)) {
+    
     let numString = commandName.slice(1, commandName.length)
     let num = Number(numString)
-    if (!!num && op === '+') humor += Number(humor)
-    else if (!!num && op ==='-') humor = humor - Number(humor)
+
+    if (!!num && op === '+') store.humor = Number(store.humor) + num
+    else if (!!num && op ==='-') store.humor = Number(store.humor) - num
+
+    console.info(`updating humor: ${commandName}, humor: ${store.humor}`)
+  } else if (commandName === '!funny') {
+    client.say(target, store.humor.toString())
   }
 }
 
+function saveHumor() {
+  console.info("saving current humor...", store.humor)
+  let data = JSON.stringify({humor: store.humor});
+  fs.writeFileSync('store/index.json', data);
+}
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
